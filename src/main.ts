0c9d22f2e73f91eb737ae5887c950c5ba216d663
nfvtw <nfvtwo@gmail.com>
2026-04-28 17:33:46 +0300

    28.04.26 17:33

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ dotenv.config();
 
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import cors from 'cors'
 
 
 async function start() {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule, {
-        cors: true
-    })
+    const app = await NestFactory.create(AppModule)
 
-    // Настройка CORS
-   app.enableCors();
+    app.use(cors({
+        credentials: true,
+        origin: '*'
+    }))
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 
 }
