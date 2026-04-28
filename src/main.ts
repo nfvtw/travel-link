1baba8e47a286b3a31087480034298d65f6ff693
nfvtw <nfvtwo@gmail.com>
2026-04-28 15:53:05 +0300

    28.04.2026 4

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,20 @@ import { AppModule } from "./app.module";
 
 async function start() {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule, {
+        cors: true
+    })
 
     // Настройка CORS
-    app.use((req, res, next) => {
-        console.log(`Request: ${req.method} ${req.url}`);
-        console.log('Origin:', req.headers.origin);
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', '*');
-        
-        if (req.method === 'OPTIONS') {
-            return res.sendStatus(204);
-        }
-        next();
+   app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+    credentials: false,
+    maxAge: 3600,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
     });
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 
